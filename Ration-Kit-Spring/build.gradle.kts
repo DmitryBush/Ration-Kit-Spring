afdcf8074d606dae55e6a68b2279f9f98415e89c
Cheremin Dmitriy <126575364+dmitriychearemin@users.noreply.github.com>
2024-04-27 15:37:25 +0300

    Add_Aspect_Library

--- Ration-Kit-Spring/build.gradle.kts
+++ Ration-Kit-Spring/build.gradle.kts
@@ -16,6 +16,15 @@ dependencies {
     implementation("org.springframework:spring-context:6.1.6")
     implementation("org.postgresql:postgresql:42.7.3")
     implementation("org.springframework:spring-aop:6.1.6")
+    implementation("org.springframework:spring-core: 6.1.6")
+    implementation("org.springframework:spring-beans: 6.1.6")
+    implementation("org.springframework:spring-context-support: 6.1.6")
+
+
+    implementation("jakarta.annotation:jakarta.annotation-api:3.0.0")
+
+    implementation("org.aspectj:aspectjrt:1.9.22")
+    implementation("org.aspectj:aspectjweaver:1.9.22")
 }
 
 tasks.test {
